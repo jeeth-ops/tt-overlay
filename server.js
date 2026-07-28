f9f35b97065d9b384823121d8092bcde8a9d8e18
jeeth-ops <chhayajeeth@gmail.com>
2026-07-28 06:53:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,20 +17,10 @@ app.get('/overlay', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    console.log('A user connected');
-
     socket.on('updateScore', (data) => {
         io.emit('liveScore', data);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
     });
 });
 
 const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-});
+server.listen(PORT);
