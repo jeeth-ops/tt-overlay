b10d6254639122b239be17917440d1bcbef72702
jeeth-ops <chhayajeeth@gmail.com>
2026-07-28 05:44:57 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Static files serve karne ke liye
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/overlay', (req, res) => {
+    res.sendFile(__dirname + '/overlay.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('updateScore', (data) => {
+        io.emit('liveScore', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+// Render ke liye sabse zaroori line: yeh automatic port utha lega
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
