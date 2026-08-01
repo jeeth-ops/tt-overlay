7ee130e0076ace11116c63c5add6b31b1a862f07
jeeth-ops <chhayajeeth@gmail.com>
2026-08-02 02:45:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -255,7 +255,8 @@ io.on('connection', async (socket) => {
     socket.on('disconnect', () => {});
 });
 
-server.listen(process.env.PORT || 3000, () => {
-    console.log(`Server running on port ${process.env.PORT || 3000}`);
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
